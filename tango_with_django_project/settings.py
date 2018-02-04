6c950d37e769ab8e87b345e7e56ba3518335fcc1
Elli Katsaouni <2344425K@student.gla.ac.uk>
2018-02-04 21:31:07 +0000

    Chapter done

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -18,7 +18,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = [STATIC_DIR, ]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -123,5 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR,]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
