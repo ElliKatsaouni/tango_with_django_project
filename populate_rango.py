8c25fa19fa6440fc075a671fb49d66047042766a
Elli Katsaouni <2344425K@student.gla.ac.uk>
2018-02-08 21:32:08 +0000

    Chapter Done

--- populate_rango.py
+++ populate_rango.py
@@ -15,36 +15,36 @@ def populate():
     # through each data structure, and add the data to our models.
 
     python_pages = [
-         {"title": "Official Python Tutorial",
-             "url":"http://docs.python.org/2/tutorial/", "views": 32},
+        {"title": "Official Python Tutorial",
+        "url":"http://docs.python.org/2/tutorial/", "views": 32},
 
-    {"title": "How to Think like a Computer Scientist",
-      "url":"http://www.greenteapress.com/thinkpython/", "views": 16},
+        {"title": "How to Think like a Computer Scientist",
+        "url":"http://www.greenteapress.com/thinkpython/", "views": 16},
 
-    {"title": "Learn Python in 10 Minutes",
-     "url":"http://www.korokithakis.net/tutorials/python/", "views": 8}]
+        {"title": "Learn Python in 10 Minutes",
+        "url":"http://www.korokithakis.net/tutorials/python/", "views": 8}]
 
 
     django_pages = [
         {"title": "Official Django Tutorial",
          "url":"https://docs.djangoproject.com/en/1.9/intro/tutorial01/", "views": 32},
 
-    {"title": "Django Rocks",
-     "url":"http://www.djangorocks.com/", "views": 16},
+        {"title": "Django Rocks",
+        "url":"http://www.djangorocks.com/", "views": 16},
 
-    {"title": "How to Tango with Django",
-     "url":"http://www.tangowithdjango.com/", "views": 8}]
+        {"title": "How to Tango with Django",
+        "url":"http://www.tangowithdjango.com/", "views": 8}]
 
     other_pages = [
         {"title": "Bottle",
-            "url":"http://bottlepy.org/docs/dev/", "views": 32},
+         "url":"http://bottlepy.org/docs/dev/", "views": 32},
 
-    {"title": "Flask",
-      "url":"http://flask.pocoo.org", "views": 16}]
+         {"title": "Flask",
+         "url":"http://flask.pocoo.org", "views": 16}]
 
 
     cats = {"Python": {"pages": python_pages, "views":128, "likes":64},
-             "Django": {"pages": django_pages, "views":64, "likes":32},
+            "Django": {"pages": django_pages, "views":64, "likes":32},
             "Other Frameworks": {"pages":other_pages, "views":32, "likes":16}}
 
     # If you want to add more catergories or pages,
--- rango/views.py
+++ rango/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from rango.models import Page
 
 def index(request):
-    #context_dict ={}
+    context_dict ={}
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {'categories': category_list,'pages' : page_list}
@@ -93,3 +93,4 @@ def add_page(request, category_name_slug):
 
     context_dict = {'form':form, 'category': category}
     return render(request, 'rango/add_page.html', context_dict)
+
