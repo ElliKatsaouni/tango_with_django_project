c7c6860c4532081b99f8c53b9ced6462ae55f878
Elli Katsaouni <2344425K@student.gla.ac.uk>
2018-02-13 15:41:03 +0000

    Chapter done

--- rango/views.py
+++ rango/views.py
@@ -205,7 +205,7 @@ def user_login(request):
 
 @login_required
 def restricted(request):
-    return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse('rango/restricted.html'))
 
 # Use the login_required() decorator to ensure only those logged in can
 # access the view.
